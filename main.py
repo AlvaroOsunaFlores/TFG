57f5c3508473821c96d4c663c73f9cf687293627
Álvaro Osuna Flores <a.osuna.2022@alumnos.urjc.es>
2025-09-30 12:44:33 +0200

    Update dbmsg : First telethon msg reciver implementation and db saved

---
+++ main.py
@@ -0,0 +1,34 @@
+import telethon
+import pymongo
+import asyncio
+
+from pymongo import MongoClient
+from telethon import TelegramClient, events
+
+TELEGRAM_API_ID = 26685793
+TELEGRAM_API_HASH = "a7b8e912e96416e885da4fdba1800d11"
+
+async def main():
+    idCount = 0
+    client = TelegramClient("study_Session", TELEGRAM_API_ID, TELEGRAM_API_HASH)
+    clientdb = MongoClient('localhost', 27017)
+    db = clientdb['Testq']
+    collection = db['CollectionTest']
+
+    await client.start()
+    @client.on(events.NewMessage)
+    async def msgHandler(event):
+        nonlocal idCount
+        msg = event.message.text
+
+        instdb = {"_id": idCount, "msg": msg}
+        try:
+            collection.insert_one(instdb)
+            print(f"Guardado en DB: {instdb}")
+        except Exception as e:
+            print("Error al guardar en MongoDB:", e)
+
+        idCount += 1
+    await client.run_until_disconnected()
+    return
+asyncio.run(main())
